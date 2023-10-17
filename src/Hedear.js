f34c4e58ef90b619501de0716bcdefba388031b3
Zia123456 <m.ziabakhteyari12@gmail.com>
2023-10-17 11:23:07 -0700

    Install material ui

--- src/Hedear.js
+++ src/Hedear.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import './Header.css';
+import Person2Icon from '@mui/icons-material/Person2';
 
 function Hedear() {
-  return <div className="header"></div>;
+  return (
+    <div className="header">
+      <Person2Icon />
+    </div>
+  );
 }
 
 export default Hedear;
