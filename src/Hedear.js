8cad2c67d489489389a74999df39c8038a2e9e2c
Zia123456 <m.ziabakhteyari12@gmail.com>
2023-10-18 10:52:53 -0700

    Add style to header

--- src/Hedear.js
+++ src/Hedear.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './Header.css';
 import Person2Icon from '@mui/icons-material/Person2';
+import IconButton from '@mui/material/IconButton';
+import Logo from './images/tinder-logo.png';
 
 function Hedear() {
   return (
     <div className="header">
-      <Person2Icon />
+      <IconButton>
+        <Person2Icon fontSize="large" className="header__icon" />
+      </IconButton>
+
+      <img className="header__logo" src={Logo} alt="logo" />
     </div>
   );
 }
