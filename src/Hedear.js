9391f925a4f9d155952d78f9cbfec43df9d4e7ef
Zia123456 <m.ziabakhteyari12@gmail.com>
2023-10-18 11:03:50 -0700

    Add forumicon

--- src/Hedear.js
+++ src/Hedear.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Header.css';
 import Person2Icon from '@mui/icons-material/Person2';
 import IconButton from '@mui/material/IconButton';
+import ForumIcon from '@mui/icons-material/Forum';
 import Logo from './images/tinder-logo.png';
 
 function Hedear() {
@@ -10,8 +11,10 @@ function Hedear() {
       <IconButton>
         <Person2Icon fontSize="large" className="header__icon" />
       </IconButton>
-
       <img className="header__logo" src={Logo} alt="logo" />
+      <IconButton>
+        <ForumIcon fontSize="large" className="header__icon" />
+      </IconButton>
     </div>
   );
 }
