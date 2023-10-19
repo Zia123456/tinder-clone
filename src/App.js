746316f43aa61e1db528e24669bc6c6d42016859
Zia123456 <m.ziabakhteyari12@gmail.com>
2023-10-19 10:59:38 -0700

    Create tindercard

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './Hedear';
+import TinderCards from './TinderCards';
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <TinderCards />
     </div>
   );
 }
